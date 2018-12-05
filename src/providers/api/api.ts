3064c5b04c68c0d2440493e7453996ca67fc3db3
Awab Abdoun <awabcodes@gmail.com>
2018-12-05 15:12:08 +0200

    added 'DECLARANTCODE' field to customs service

--- src/providers/api/api.ts
+++ src/providers/api/api.ts
@@ -1,29 +1,19 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from "@angular/common/http";
+import { Injectable } from "@angular/core";
 
 /**
  * Api is a generic REST Api handler. Set your API url first.
  */
 @Injectable()
 export class Api {
- 
- public urlip: string ='mobappszone.xyz:8080';
- //  public urlip: string ='192.168.98.120:8080';
-public  url: string = 'https://'+this.urlip+'/api';
+  //  public urlip: string ='mobappszone.xyz:8080';
+  //  public urlip: string ='192.168.98.120:8080';
+  public urlip = "127.0.0.1:8080";
+  public url: string = "http://" + this.urlip + "/api";
 
-
-
-
-  constructor(public http: HttpClient) {
-
-
-
-  }
+  constructor(public http: HttpClient) {}
 
   get(endpoint: string, params?: any, reqOpts?: any) {
-
-
-
     if (!reqOpts) {
       reqOpts = {
         params: new HttpParams()
@@ -37,27 +27,25 @@ public  url: string = 'https://'+this.urlip+'/api';
     //     reqOpts.params.set(k, params[k]);
     //   }
     // }
-if (params) {
-  endpoint=endpoint+params
-}
-    return this.http.get(this.url + '/' + endpoint,reqOpts );
+    if (params) {
+      endpoint = endpoint + params;
+    }
+    return this.http.get(this.url + "/" + endpoint, reqOpts);
   }
 
   post(endpoint: string, body: any, reqOpts?: any) {
-
-
-    return this.http.post(this.url + '/' + endpoint, body, reqOpts);
+    return this.http.post(this.url + "/" + endpoint, body, reqOpts);
   }
 
   put(endpoint: string, body: any, reqOpts?: any) {
-    return this.http.put(this.url + '/' + endpoint, body, reqOpts);
+    return this.http.put(this.url + "/" + endpoint, body, reqOpts);
   }
 
   delete(endpoint: string, reqOpts?: any) {
-    return this.http.delete(this.url + '/' + endpoint, reqOpts);
+    return this.http.delete(this.url + "/" + endpoint, reqOpts);
   }
 
   patch(endpoint: string, body: any, reqOpts?: any) {
-    return this.http.put(this.url + '/' + endpoint, body, reqOpts);
+    return this.http.put(this.url + "/" + endpoint, body, reqOpts);
   }
 }

e84b29038040d49ef26e4cc9b302d337c4b3f0f5
Awab Abdoun <awabcodes@gmail.com>
2018-12-16 10:51:43 +0200

    phone number validation

--- src/pages/signup/signup.ts
+++ src/pages/signup/signup.ts
@@ -1,88 +1,98 @@
-import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { IonicPage, NavController, ToastController,LoadingController } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { TranslateService } from "@ngx-translate/core";
+import {
+  IonicPage,
+  NavController,
+  ToastController,
+  LoadingController
+} from "ionic-angular";
 
-import { User } from '../../providers/providers';
-import { MainPage } from '../pages';
+import { User } from "../../providers/providers";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @IonicPage()
 @Component({
-  selector: 'page-signup',
-  templateUrl: 'signup.html'
+  selector: "page-signup",
+  templateUrl: "signup.html"
 })
 export class SignupPage {
-  // The account fields for the login form.
-  // If you're using the username field with or without email, make
-  // sure to add it to the type
-  account: { username: string,login :string, password: string } = {
-    username: '',
-    login:'',
-    password: 'test'
+  account: { username: string; login: string; password: string } = {
+    username: "",
+    login: "",
+    password: "test"
   };
 
-  // Our translated text strings
+  private signup: FormGroup;
+  submitAttempt: boolean = false;
+
   private signupErrorString: string;
 
-  constructor(public navCtrl: NavController,
+  constructor(
+    public navCtrl: NavController,
     public user: User,
+    private formBuilder: FormBuilder,
     public loadingCtrl: LoadingController,
     public toastCtrl: ToastController,
-    public translateService: TranslateService) {
+    public translateService: TranslateService
+  ) {
+    this.signup = this.formBuilder.group({
+      PHONENUMBER: [
+        "",
+        Validators.compose([
+          Validators.required,
+          Validators.minLength(9),
+          Validators.maxLength(9),
+          Validators.pattern("[0-9]*")
+        ])
+      ]
+    });
 
-    this.translateService.get('SIGNUP_ERROR').subscribe((value) => {
+    this.translateService.get("SIGNUP_ERROR").subscribe(value => {
       this.signupErrorString = value;
-    })
+    });
   }
 
   doSignup() {
     // Attempt to login in through our User service
-   
-    let loader = this.loadingCtrl.create({
-      content: "Please wait..."
-    });
-    loader.present();
-    this.account.password=this.account.username;
-    this.account.login=this.account.username;
-    this.user.login(this.account).subscribe((resp) => {
-      loader.dismiss();
-         let toast = this.toastCtrl.create({
-        message: this.signupErrorString,
-        duration: 3000,
-        position: 'top'
-      });
-      toast.present();
-  
-     }, err => {
-      this.user.sendOtp(this.account).subscribe((res: any) => {
-        loader.dismiss();
-        if(res.success==true){
-  
-          this.navCtrl.setRoot('VlidateOtpPage',{"username" : this.account.username,"OtpType":"signup"});
-        }
-     }  , err => {
-      console.error('ERROR', err);
-    }
-    
-    );
-    
-    });
-   
+    this.submitAttempt = true;
 
-    // this.user.signup(this.account).subscribe((resp) => {
-    //
-    //
-    //   this.navCtrl.push(MainPage);
-    // }, (err) => {
-    //
-    //   this.navCtrl.push(MainPage);
-    //
-    //   // Unable to sign up
-    //   let toast = this.toastCtrl.create({
-    //     message: this.signupErrorString,
-    //     duration: 3000,
-    //     position: 'top'
-    //   });
-    //   toast.present();
-    // });
+    if (this.signup.valid) {
+      let loader = this.loadingCtrl.create({
+        content: "Please wait..."
+      });
+      loader.present();
+      this.account.password = this.signup.controls["PHONENUMBER"].value;
+      this.account.login = this.signup.controls["PHONENUMBER"].value;
+      this.user.login(this.account).subscribe(
+        resp => {
+          loader.dismiss();
+          let toast = this.toastCtrl.create({
+            message: this.signupErrorString,
+            duration: 3000,
+            position: "top"
+          });
+          toast.present();
+          this.submitAttempt = false;
+        },
+        err => {
+          this.user.sendOtp(this.account).subscribe(
+            (res: any) => {
+              loader.dismiss();
+              if (res.success == true) {
+                this.navCtrl.setRoot("VlidateOtpPage", {
+                  username: this.account.username,
+                  OtpType: "signup"
+                });
+              }
+              this.submitAttempt = false;
+            },
+            err => {
+              console.error("ERROR", err);
+              this.submitAttempt = false;
+            }
+          );
+        }
+      );
+    }
   }
 }

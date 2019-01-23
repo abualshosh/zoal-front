b33b032df76a03d91beb2e0f5c2b4507b59c69cc
Awab Abdoun <awabcodes@gmail.com>
2019-01-23 09:11:27 +0200

    transaction detail (receipt) enhancements

--- src/pages/payment-pages/transaction-detail/transaction-detail.ts
+++ src/pages/payment-pages/transaction-detail/transaction-detail.ts
@@ -15,6 +15,7 @@ import { TranslateService } from "@ngx-translate/core";
 export class TransactionDetailPage {
   public items: any[] = [];
   public main: any[] = [];
+
   constructor(
     public viewCtrl: ViewController,
     public navCtrl: NavController,
@@ -31,10 +32,26 @@ export class TransactionDetailPage {
           this.items[i].tranCurrency = values["SDG"];
         });
       }
+
+      if (this.items[i].Card) {
+        this.items[i].Card = this.items[i].Card.replace(
+          this.items[i].Card.substring(6, this.items[i].Card.length - 3),
+          "***********"
+        );
+      } else if (this.items[i].WalletNumber) {
+        this.items[i].WalletNumber = this.items[i].WalletNumber.replace(
+          this.items[i].WalletNumber.substring(
+            6,
+            this.items[i].WalletNumber.length - 3
+          ),
+          "*******"
+        );
+      }
     }
   }
 
   ionViewDidLoad() {}
+
   dismiss() {
     this.viewCtrl.dismiss();
   }

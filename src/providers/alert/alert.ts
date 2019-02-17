650067be9d6c1c474106e9871aedca8e8067c50b
Awab Abdoun <awabcodes@gmail.com>
2019-02-17 11:33:39 +0200

    improved alert provider

--- src/providers/alert/alert.ts
+++ src/providers/alert/alert.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { AlertController } from "ionic-angular";
+import { AlertController, ToastController } from "ionic-angular";
 import { TranslateService } from "@ngx-translate/core";
 
 @Injectable()
@@ -10,9 +10,14 @@ export class AlertProvider {
 
   constructor(
     public alertCtrl: AlertController,
+    public toastCtrl: ToastController,
     public translateService: TranslateService
   ) {
-    translateService
+    this.getTranslatedStrings();
+  }
+
+  getTranslatedStrings() {
+    this.translateService
       .get(["errorTitle", "errorMessage", "errorButton"])
       .subscribe(values => {
         this.errorTitle = values["errorTitle"];
@@ -21,10 +26,20 @@ export class AlertProvider {
       });
   }
 
-  showAlert(data: any) {
+  showAlert(data: any, showExact?: boolean) {
+    this.getTranslatedStrings();
+
     let message: any;
     if (data.responseCode != null) {
       message = data.responseMessage;
+    } else if (showExact) {
+      this.translateService.get(data).subscribe(val => {
+        if (val) {
+          message = val;
+        } else {
+          message = data;
+        }
+      });
     } else {
       message = this.errorMessage;
     }
@@ -36,5 +51,21 @@ export class AlertProvider {
       cssClass: "alertCustomCss"
     });
     alert.present();
+  }
+
+  showToast(message) {
+    this.translateService.get(message).subscribe(val => {
+      if (val) {
+        message = val;
+      } else {
+        message = message;
+      }
+    });
+    let toast = this.toastCtrl.create({
+      message: message,
+      duration: 3000,
+      position: "top"
+    });
+    toast.present();
   }
 }

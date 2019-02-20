2f8be2b3c1d71aa7024ff734b9717d292e7cfef9
Awab Abdoun <awabcodes@gmail.com>
2019-02-20 14:02:49 +0200

    implemented storage methods

--- src/providers/storage/storage.ts
+++ src/providers/storage/storage.ts
@@ -9,8 +9,20 @@ export interface Wallet {
   number: string;
   modified: number;
 }
+export class Item {
+  constructor(
+    public id: number,
+    public name?: string,
+    public walletNumber?: string,
+    public cardNumber?: string,
+    public expDate?: string,
+    public favoriteText?: string
+  ) {}
+}
 
 const WALLETS_KEY = "wallets";
+
+const CARDS_KEY = "cards";
 
 const PROFILE_KEY = "profile";
 
@@ -18,39 +30,47 @@ const PROFILE_KEY = "profile";
 export class StorageProvider {
   constructor(private storage: Storage) {}
 
-  // CREATE
-  addItem(item: Wallet): Promise<any> {
-    return this.storage.get(WALLETS_KEY).then((items: Wallet[]) => {
-      if (items) {
-        items.push(item);
-        return this.storage.set(WALLETS_KEY, items);
-      } else {
-        return this.storage.set(WALLETS_KEY, [item]);
-      }
-    });
-  }
-
   setProfile(profile): Promise<any> {
     return this.storage.set(PROFILE_KEY, profile);
-  }
-
-  // READ
-  getItems(): Promise<Wallet[]> {
-    return this.storage.get(WALLETS_KEY);
   }
 
   getProfile(): Observable<any> {
     return Observable.fromPromise(this.storage.get(PROFILE_KEY));
   }
 
+  getWallets(): Promise<Item[]> {
+    return this.storage.get(WALLETS_KEY);
+  }
+
+  getCards(): Promise<Item[]> {
+    return this.storage.get(CARDS_KEY);
+  }
+
+  // CREATE
+  addItem(item, key): Promise<any> {
+    return this.storage.get(key).then(items => {
+      if (items) {
+        items.push(item);
+        return this.storage.set(key, items);
+      } else {
+        return this.storage.set(key, [item]);
+      }
+    });
+  }
+
+  // READ
+  getItemss(key: string): Promise<any[]> {
+    return this.storage.get(key);
+  }
+
   // UPDATE
-  updateItem(item: Wallet): Promise<any> {
-    return this.storage.get(WALLETS_KEY).then((items: Wallet[]) => {
+  updateItem(item, key): Promise<any> {
+    return this.storage.get(key).then(items => {
       if (!items || items.length === 0) {
         return null;
       }
 
-      let newItems: Wallet[] = [];
+      let newItems: any[] = [];
 
       for (let i of items) {
         if (i.id === item.id) {
@@ -60,25 +80,25 @@ export class StorageProvider {
         }
       }
 
-      return this.storage.set(WALLETS_KEY, newItems);
+      return this.storage.set(key, newItems);
     });
   }
 
   // DELETE
-  deleteItem(id: number): Promise<Wallet> {
-    return this.storage.get(WALLETS_KEY).then((items: Wallet[]) => {
+  deleteItem(id: number, key): Promise<any> {
+    return this.storage.get(key).then(items => {
       if (!items || items.length === 0) {
         return null;
       }
 
-      let toKeep: Wallet[] = [];
+      let toKeep: any[] = [];
 
       for (let i of items) {
         if (i.id !== id) {
           toKeep.push(i);
         }
       }
-      return this.storage.set(WALLETS_KEY, toKeep);
+      return this.storage.set(key, toKeep);
     });
   }
 }

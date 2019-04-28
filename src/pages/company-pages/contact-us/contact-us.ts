64ac91a8e172b4f7cdc034459931062a917cd84b
Awab Abdoun <awabcodes@gmail.com>
2019-04-28 14:42:31 +0200

    contact-us page new backend integration

--- src/pages/company-pages/contact-us/contact-us.ts
+++ src/pages/company-pages/contact-us/contact-us.ts
@@ -7,10 +7,8 @@ import {
   ViewController
 } from "ionic-angular";
 import { Validators, FormBuilder, FormGroup } from "@angular/forms";
-import { GetServicesProvider } from "../../../providers/providers";
 import { AlertProvider } from "../../../providers/alert/alert";
-import * as moment from "moment";
-import { StorageProvider } from "../../../providers/storage/storage";
+import { Api } from "../../../providers/api/api";
 
 @IonicPage()
 @Component({
@@ -20,22 +18,16 @@ import { StorageProvider } from "../../../providers/storage/storage";
 export class ContactUsPage {
   private contactForm: FormGroup;
   submitAttempt: boolean = false;
-  profile: any;
 
   constructor(
     public navCtrl: NavController,
     private formBuilder: FormBuilder,
-    public serviceProvider: GetServicesProvider,
     public alertProvider: AlertProvider,
     public modalCtrl: ModalController,
     public viewCtrl: ViewController,
-    public storageProvider: StorageProvider,
+    public api: Api,
     public navParams: NavParams
   ) {
-    this.storageProvider.getProfile().subscribe(val => {
-      this.profile = val;
-    });
-
     this.contactForm = this.formBuilder.group({
       title: [
         "",
@@ -47,8 +39,6 @@ export class ContactUsPage {
       ]
     });
   }
-
-  ionViewDidLoad() {}
 
   clearInput() {
     this.contactForm.controls["title"].reset();
@@ -62,15 +52,13 @@ export class ContactUsPage {
       this.alertProvider.showLoading();
 
       let request = this.contactForm.value;
-      request.userName = this.profile.userName;
-      request.date = moment().format("YYYY-MM-DD HH:MM:SS");
-
-      this.serviceProvider.load(request, "consumer/user-messages").then(
+      this.api.post("user-messages", request).subscribe(
         res => {
           this.alertProvider.hideLoading();
           this.alertProvider.showToast("companyContactSuccess");
           this.contactForm.reset();
           this.submitAttempt = false;
+          this.cancel();
         },
         err => {
           this.alertProvider.hideLoading();

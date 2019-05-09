69836c87a9975d50a9a25dd07f2f979d56535715
Awab Abdoun <awabcodes@gmail.com>
2019-05-09 09:46:13 +0200

    deleted settings page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { IonicStorageModule, Storage } from "@ionic/storage";
 import { TranslateLoader, TranslateModule } from "@ngx-translate/core";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
-import { Settings } from "../providers/providers";
 import { User } from "../providers/providers";
 import { Api } from "../providers/providers";
 import { GetServicesProvider } from "../providers/providers";
@@ -44,21 +43,6 @@ import { Screenshot } from "@ionic-native/screenshot";
 // in Ionic's static asset pipeline.
 export function createTranslateLoader(http: HttpClient) {
   return new TranslateHttpLoader(http, "./assets/i18n/", ".json");
-}
-
-export function provideSettings(storage: Storage) {
-  /**
-   * The Settings provider takes a set of default settings for your app.
-   *
-   * You can add new settings options at any time. Once the settings are saved,
-   * these values will not overwrite the saved values (this can be done manually if desired).
-   */
-  return new Settings(storage, {
-    option1: true,
-    option2: "Ionitron J. Framework",
-    option3: "3",
-    option4: "Hello"
-  });
 }
 
 @NgModule({
@@ -115,7 +99,6 @@ export function provideSettings(storage: Storage) {
       useClass: AuthExpiredInterceptor,
       multi: true
     },
-    { provide: Settings, useFactory: provideSettings, deps: [Storage] },
     // Keep this to enable Ionic's runtime error handling during development
     { provide: ErrorHandler, useClass: IonicErrorHandler },
     StorageProvider,
--- src/providers/providers.ts
+++ src/providers/providers.ts
@@ -1,5 +1,4 @@
 import { Api } from "./api/api";
-import { Settings } from "./settings/settings";
 import { User } from "./user/user";
 import { AuthService } from "./auth/auth";
 import { AuthInterceptor } from "./auth/auth.interceptor";
@@ -7,7 +6,6 @@ import { AuthExpiredInterceptor } from "./auth/auth-expired.interceptor";
 import { GetServicesProvider } from "./get-services/get-services";
 export {
   Api,
-  Settings,
   User,
   AuthService,
   AuthInterceptor,

7447a3901ead66ff6033087f6a74ecf01ee603c5
Awab Abdoun <awabcodes@gmail.com>
2019-12-19 12:05:46 +0200

    automatic validate otp

--- src/pages/user-pages/vlidate-otp/vlidate-otp.ts
+++ src/pages/user-pages/vlidate-otp/vlidate-otp.ts
@@ -1,9 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { User } from "../../../providers/providers";
 import { MainPage } from "../../pages";
 import { TranslateService } from "@ngx-translate/core";
 import { AlertProvider } from "../../../providers/alert/alert";
+
+declare var SMSReceive: any;
 
 @IonicPage()
 @Component({
@@ -11,9 +13,19 @@ import { AlertProvider } from "../../../providers/alert/alert";
   templateUrl: "vlidate-otp.html"
 })
 export class VlidateOtpPage {
+  @ViewChild('otp1') otp1;
+  @ViewChild('otp2') otp2;
+  @ViewChild('otp3') otp3;
+  @ViewChild('otp4') otp4;
+
+  otpchar1 = "";
+  otpchar2 = "";
+  otpchar3 = "";
+  otpchar4 = "";
+
   account: { login: string; otp: string } = {
     login: "",
-    otp: "test"
+    otp: ""
   };
 
   otpType: any;
@@ -27,9 +39,50 @@ export class VlidateOtpPage {
   ) {
     this.account.login = navParams.get("username");
     this.otpType = navParams.get("OtpType");
+    this.start()
+  }
+
+  ionViewDidLoad() {
+    setTimeout(() => {
+      this.otp1.setFocus();
+    }, 150);
+  }
+
+  start() {
+    if (typeof SMSReceive !== 'undefined') {
+      SMSReceive.startWatch(
+        () => {
+          document.addEventListener('onSMSArrive', (e: any) => {
+            this.processSMS(e.data);
+            this.stopWatch();
+          });
+        },
+        () => { console.log('watch start failed'); }
+      )
+    }
+  }
+
+  stopWatch() {
+    SMSReceive.stopWatch(
+      () => { console.log('watch stopped') },
+      () => { console.log('watch stop failed') }
+    )
+  }
+
+  processSMS(data) {
+    const message = data.body;
+    if (message && message.indexOf('ZoalPay') != -1) {
+      this.account.otp = data.body.slice(0, 4);
+      this.populateOtpFields();
+      this.validateOtp();
+    }
   }
 
   validateOtp() {
+    if (!this.account.otp) {
+      this.account.otp = this.otpchar1 + this.otpchar2 + this.otpchar3 + this.otpchar4;
+    }
+
     this.alertProvider.showLoading();
 
     this.user.validateOtp(this.account).subscribe(
@@ -43,11 +96,69 @@ export class VlidateOtpPage {
       },
       err => {
         this.alertProvider.hideLoading();
-        this.alertProvider.showAlert(err);
+        this.alertProvider.showToast(err);
       },
       () => {
         this.alertProvider.hideLoading();
       }
     );
+  }
+
+  resendOtp() {
+    this.account.otp = "";
+    if (this.otpType === "login") {
+      this.resendLoginOtp()
+    } else {
+      this.resendSignupOtp();
+    }
+  }
+
+  resendLoginOtp() {
+    this.user.sendOtpLogin(this.account).subscribe(
+      (res: any) => {
+        if (res.success) {
+          this.alertProvider.showToast("sentOtp")
+          this.start();
+        } else {
+          this.alertProvider.showToast("failedToSendOTP");
+        }
+      },
+      err => {
+        this.alertProvider.hideLoading();
+      },
+      () => {
+        this.alertProvider.hideLoading();
+      }
+    );
+  }
+
+  resendSignupOtp() {
+    this.user.sendOtpSignup(this.account).subscribe(
+      (res: any) => {
+        if (res.success == true) {
+          this.alertProvider.showToast("sentOtp")
+          this.start();
+        } else {
+          this.alertProvider.showToast("failedToSendOTP");
+        }
+      },
+      err => {
+        this.alertProvider.hideLoading();
+      },
+      () => {
+        this.alertProvider.hideLoading();
+      }
+    );
+  }
+
+  populateOtpFields() {
+    this.otpchar1 = this.account.otp[0]
+    this.otpchar2 = this.account.otp[1]
+    this.otpchar3 = this.account.otp[2]
+    this.otpchar4 = this.account.otp[3]
+  }
+
+  moveFocus(nextElement) {
+    nextElement.setFocus();
   }
 }
